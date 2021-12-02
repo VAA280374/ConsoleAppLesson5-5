cf1cd0c3ef226670b7011bf0c941b2021b28daa8
alexander.a.volkov@rambler.ru <alexander.a.volkov@rambler.ru>
2021-12-02 07:38:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppLesson5-5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLesson5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vN, vi, vj;
+            Console.Write("Введите число N=");
+            vN = Convert.ToInt16(Console.ReadLine());
+            int[,] arrey = new int[vN,vN];
+            for (int i = 0; i < vN; i++)
+            {
+                for (int j = 0; j < vN; j++)
+                {
+                    vi = i / 2;
+                    vj = j / 2;
+                    if ((2 * vi - i) == (2 * vj - j)) arrey[i, j] = 1;
+                    else arrey[i, j] = 0;
+                    Console.Write("{0} ", arrey[i, j]);
+                }
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
